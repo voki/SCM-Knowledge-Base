bc50457b7480e09f2f0ab33083ff47e68a1e7ddf
Piotr Szwed <pszwed@gmail.com>
2014-01-21 16:46:57 +0100

    New template initialization

--- conf.py
+++ conf.py
@@ -33,7 +33,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
