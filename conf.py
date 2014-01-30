b6cc8b70468593843db7bdda4d72faef0dd52ed1
szwed <pszwed@gmail.com>
2014-01-30 15:24:19 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'scm_and_devops_knowledge_base'
+project = u'scm_knowledge_base'
 copyright = u'2014, SCM Forge'
 
 # The version info for the project you're documenting, acts as replacement for
